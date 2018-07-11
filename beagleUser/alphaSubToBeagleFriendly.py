2e9b42cf299598f44a819e71eac942736ded29a0
NoiSe <marco23.p@gmail.com>
2018-07-11 12:36:28 +0200

    completed argparse

--- beagleUser/alphaSubToBeagleFriendly.py
+++ beagleUser/alphaSubToBeagleFriendly.py
@@ -1,17 +1,37 @@
+import argparse
+import pickle
+
 import numpy as np
 import pandas as pd
 
-alphastring = ""
-bearstring = ""
 
-alphamap = {}
+parser = argparse.ArgumentParser()
+
+##parser definition
+parser.add_argument("-m", "--matrix", help="the substitution matrix path, in a pickled dataframe",)
+parser.add_argument("-a", "--alphamap", help="the mapping path, in a pickled dict",)
+parser.add_argument("-v", "--verbose", help="toggle verbosity", action="store_true")
+
+args = parser.parse_args()
+if (not args.matrix) or (not args.alphamap):
+	raise OSError('\n-m (subs matrix df pickle) and -a (alphabet mapping in dict pickle) are required!\n')
+
+#variable reading
+alphasub = pickle.load(open(args.matrix, 'rb'))
+
+alphastring = "".join(list(alphasub.index))
+bearstring = "abcdefghi=lmnopqrstuvwxyz^!\"#$%&\'()+234567890>[]:ABCDEFGHIJKLMNOPQRSTUVW{YZ~?_|/\\}@"
+
+
+alphamap = pickle.load(open(args.alphamap, 'rb'))
+
 
 Lalpha = len(alphastring)
 Lbear = len(bearstring)
 
-alphasub = pd.read_table(alphasubpath)
 alphabearsub = np.zeros((Lbear,Lbear))
 
+#filling
 for ix, bearchar_i in enumerate(bearstring):
 	for jx, bearchar_j in enumerate(bearstring):
 		alphaix = alphastring.index(alphamap[bearchar_i])
@@ -20,9 +40,11 @@ for ix, bearchar_i in enumerate(bearstring):
 		alphascore = alphasub.iloc[alphaix, alphajx]
 		alphabearsub[ix,jx] = alphascore
 
-###TODO save or print the matrix in beagle way
+###saving the matrix in beagle way
 print(alphabearsub)
 with open('convertedMatrix','w') as f:
-	for row in alphabearsub:
+	for i, row in enumerate(alphabearsub):
+		if args.verbose:
+			print(f'filling row {i}')
 		for value in row:
 			f.write(value)
